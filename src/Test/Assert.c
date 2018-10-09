277db8388e5f5535a493d6981d53b86fcec858ac
Felix Schlitter <felix@sylo.io>
2018-10-09 14:19:45 +1300

    Use 'purs_assert' for assertions

--- src/Test/Assert.c
+++ src/Test/Assert.c
@@ -1,14 +1,12 @@
 #include <purescript.h>
 #include "Effect.h"
 
-EFFECT_FUNC_2(Test_Assert_assert$, msg, cond, {
-	if (*purs_any_get_int(cond) != 1) {
-		return Effect_error(msg);
-	}
-	return (const purs_any_t *) NULL;
-})
+PURS_FFI_FUNC_3(Test_Assert_assert$, msg, cond, _, {
+	purs_assert(purs_any_get_int(cond), "%s", purs_any_get_string(msg));
+	return NULL;
+});
 
-EFFECT_FUNC_1(Test_Assert_checkThrows, f, {
+PURS_FFI_FUNC_2(Test_Assert_checkThrows, f, _, {
 	const purs_any_t * r = purs_any_app(f, NULL);
-	return Effect_is_error(r);
-})
+	return purs_any_bool(Effect_is_error(r));
+});
