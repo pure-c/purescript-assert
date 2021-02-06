1cfc9e24cd6922a46b267da0e056dccb41d855b1
Felix Schlitter <felixschlitter@gmail.com>
2021-02-06 17:01:54 +1300

    Update for purec#60

---
+++ spago.dhall
@@ -0,0 +1,5 @@
+{ name = "assert"
+, dependencies = [ "prelude", "effect", "console" ]
+, packages = ../pure-c/package-sets/packages.dhall
+, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+}
--- src/Test/Assert.c
+++ src/Test/Assert.c
@@ -1,12 +1,14 @@
 #include <purescript.h>
-#include "Effect.h"
 
-PURS_FFI_FUNC_3(Test_Assert_assert$, msg, cond, _, {
-	purs_assert(purs_any_get_int(cond), "%s", purs_any_get_string(msg));
-	return NULL;
-});
+PURS_FFI_FUNC_3(Test_Assert_assert$, msg, cond, _) {
+	const purs_str_t *str = purs_any_get_string(msg);
+	purs_assert(purs_any_get_int(cond), "%s", str->data);
+	return purs_any_null;
+}
 
-PURS_FFI_FUNC_2(Test_Assert_checkThrows, f, _, {
-	const purs_any_t * r = purs_any_app(f, NULL);
-	return purs_any_bool(Effect_is_error(r));
-});
+// There's no concept of "throwing exceptions" in C.
+// We still evaluate the fn.
+PURS_FFI_FUNC_2(Test_Assert_checkThrows, f, _) {
+	const purs_any_t r = purs_any_app(f, purs_any_null);
+	return purs_any_false;
+}
